365ff88a898d5d6eafe5e42a8be62ca2708443f3
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-05 22:14:52 -0300

    Create Aula 0 de Python.py

---
+++ Aula 0 de Python.py
@@ -0,0 +1,28 @@
+'roxo'
+'Nicolas'
+'OutKast'
+
+print('Boas vindas a aula de Python')
+
+nome = 'Nicolas'
+cor_favorita = 'roxo'
+print(nome, cor_favorita)
+
+saldo = 5000
+saldo = saldo + 2000
+print('Saldo na sua conta: R$', saldo)
+
+saldo = saldo * 2
+
+import this  
+
+idade = 18
+
+# se idade for maior que 18
+if idade >= 18:
+    print('Pode dirigir')
+# senão
+else:
+    print('Não pode dirigir')
+
+# isso é um comentário
